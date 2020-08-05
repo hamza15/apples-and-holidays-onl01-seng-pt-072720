c496467957edcfaff071bd2c8a8047675897b6da
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-08-05 02:39:00 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -73,10 +73,8 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   holiday_hash.each do |season, holiday|
-    holiday.each do |supply|
-      if supply == "BBQ"
-        puts holiday
-      end
+    season.each do |supply|
+        puts supply
     end
   end
 end
